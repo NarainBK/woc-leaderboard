528f0ac08d1d831d8643df839685a92d5ce6d5fa
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-12-20 10:45:07 +0530

    NEXT_AUTH object complete

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,16 +1,39 @@
-// import Github from "next-auth/providers/github";
+import Github from "next-auth/providers/github";
+import prisma from "../db";
 
-// export const NEXT_AUTH = {
-//   providers: [
-//     Github({
-//       clientId: process.env.GITHUB_CLIENT_ID || "",
-//       clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
-//     }),
-//   ],
-//   secret: process.env.NEXTAUTH_SECRET,
-//   callbacks: {
-//     async signIn({ user, account, profile }: any) {
-//       return true;
-//     }
-//   }
-// }
+export const NEXT_AUTH = {
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ profile }: any) {
+      const username = profile.login;
+      try {
+        const check = await prisma.participant.findFirst({
+          where: {
+            username: username
+          }
+        });
+        // Account does not exist or banned account
+        if (!check || check.accountActive === false) {
+          return false;
+        }
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+    async session({ session, token }: any) {
+      if (session && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    }
+  }
+}
+
+// TODO: Add Rate Limit to the login endpoint
